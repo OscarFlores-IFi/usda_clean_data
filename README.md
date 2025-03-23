eae21e2ca0628aee938e19cdcc1f3c8153f34d23
Oscar <oscar-eduardo.flores-hernandez@datanalytics-da.de>
2025-03-23 17:56:02 +0100

    pivot nutrients

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # usda_clean_data
 Making a usable dataset out of USDA. 
+
+USDA contains pretty nice dataset, but sometimes is not straightforward to deal with their dataset. Plus, it contains some irregularities. 
+
+Through this process, it is assumed that all the data is stored under the folder /data
+![usda extracted data](image.png)
+
+For this code the used dataset is the one of "all data types". 
+
+
+Documentation of USDA Foundation Foods:
+https://fdc.nal.usda.gov/Foundation_Foods_Documentation
+
+Link to download datasets:
+https://fdc.nal.usda.gov/download-datasets
+
+FOUNDATION FOODS:
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_foundation_food_csv_2024-10-31.zip
+
+SR LEGACY:
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_sr_legacy_food_csv_2018-04.zip
+
+FOOD AND NUTRIENT DATABASE FOR DIETARY STUDIES:
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_survey_food_csv_2024-10-31.zip
+
+BRANDED FOODS:
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_branded_food_csv_2024-10-31.zip
+
+ALL DATA TYPES:
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_csv_2024-10-31.zip
+
---
+++ clean_data.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+
+data_folder = "data"  # Replace with your actual data folder path
+
+directory_path = os.path.join(data_folder)
+print(directory_path)
+food = pd.read_csv(os.path.join(directory_path, 'food.csv'))
+food_nutrient = pd.read_csv(os.path.join(directory_path, 'food_nutrient.csv'), low_memory=False)
+nutrient = pd.read_csv(os.path.join(directory_path, 'nutrient.csv'))
+
+# pre-process food names and categories
+food_category = pd.read_csv(os.path.join(directory_path, 'food_category.csv'))
+food_category = food_category.rename(columns={"description": "CATEGORY"})
+
+food = food.drop_duplicates(subset=["fdc_id","data_type","description","food_category_id"])
+print(f"In total, {food["fdc_id"].nunique()} unique products were found")
+
+# pre-process food nutrients
+#### WHAT ABOUT THE DUPLICATES?? (AROUND 2400, ALL BRANDED FOODS, WHAT SHOULD WE DO WITH THEM?)
+# By now, let's assume that the latest is correct.
+food_nutrient = food_nutrient.drop_duplicates(subset=["fdc_id", "nutrient_id"], keep="last")
+
+merged_nutrients = food_nutrient[["fdc_id", "nutrient_id", "amount"]] \
+    .merge(nutrient[["id", "name", "unit_name", "nutrient_nbr"]],
+            left_on='nutrient_id', right_on='id', how="left") \
+    .merge(food[["fdc_id", "data_type", "description", "food_category_id"]],
+            left_on="fdc_id", right_on="fdc_id", how="right")
+
+merged_nutrients = merged_nutrients.rename(columns={
+    'description': 'FOOD_NAME',
+    'name': 'NUTRIENT',
+    'amount': 'value',
+    'unit_name': 'UNIT',
+    'data_type': 'DATA_TYPE'})
+
+food_category['id'] = food_category['id'].astype(str)
+merged_nutrients['food_category_id'] = merged_nutrients['food_category_id'].astype(str)
+
+merged_nutrients = merged_nutrients.merge(food_category[["id", "CATEGORY"]],
+                                                left_on='food_category_id', right_on='id', how="left")
+
+desired_columns = ['fdc_id', 'DATA_TYPE', 'CATEGORY', 'FOOD_NAME', 'NUTRIENT', 'UNIT', 'value']
+merged_nutrients = merged_nutrients[desired_columns]
+
+pivoted = merged_nutrients.pivot(index=['fdc_id', 'DATA_TYPE', 'CATEGORY', 'FOOD_NAME'], columns=['NUTRIENT', 'UNIT'], values=['value'])
+
+pivoted_copy = pd.DataFrame(pivoted.values, columns=[str(i[1]) + '(' + str(i[2]) + ')' for i in pivoted.columns], index=pivoted.index)
+pivoted_copy.to_csv("combined_pivoted_data.csv")
+
+print(f"After processing, {pivoted_copy.shape[0]} products, with {pivoted_copy.shape[1]} columns")
